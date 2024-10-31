dd075e8bcacddd53d7f4ae1cb792bf6b19afe83b
Huffer <hugoferreirait@gmail.com>
2024-10-31 10:59:11 -0300

    add eslint parsers

--- server.js
+++ server.js
@@ -11,8 +11,30 @@ app.post('/', async (req, res) => {
   const { file, fileName } = req.body;
 
   const eslint = new ESLint({
-    overrideConfigFile: true,
+    useEslintrc: false,
     overrideConfig: {
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      env: {
+        es2022: true,
+        node: true,
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
       rules: {
         eqeqeq: 'error',
         'no-else-return': 'error',
@@ -33,7 +55,6 @@ app.post('/', async (req, res) => {
     };
   });
 
-  console.log('file', req.body);
   res.status(200).json({ issues });
 });
 
@@ -41,3 +62,31 @@ app.post('/', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const test = JSON.stringify(`
+interface Props {
+  data: string
+}
+
+function sample(): Props {
+  const CONSTANT_INSIDE_COMPONENT = 'it-should fail';
+}
+
+export const ReactComponent = (props: Props) => {
+  const abc = 'abc' + testConst;
+  var newVar = '';
+  const testFunc = elseIfReturnFunction();
+
+  const snake_case_const = '';
+
+  // Else Return error statement
+  const newFunc = (x) => {
+    if (x) return;
+    else return '1';
+  };
+
+  return <div>test</div>;
+};
+`);
+
+console.log('test', test);
